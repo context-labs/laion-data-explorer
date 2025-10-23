2bf33fd9a8162130a960f2f330392ad5058986fe
Sean <sean@kuzco.xyz>
2025-10-22 19:53:54 -0700

    refactor: Simplify Skeleton component animation with `animate-pulse` class

--- frontend/src/ui/components/ui/Skeleton.tsx
+++ frontend/src/ui/components/ui/Skeleton.tsx
@@ -6,16 +6,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn(
-        `
-          relative overflow-hidden rounded-md bg-skeleton
-
-          before:absolute before:inset-0 before:-translate-x-full
-          before:animate-shimmer before:bg-gradient-to-r before:from-transparent
-          before:via-white/10 before:to-transparent
-        `,
-        className,
-      )}
+      className={cn("animate-pulse rounded-md bg-skeleton", className)}
       {...props}
     />
   );
